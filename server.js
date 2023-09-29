3453c420a403b86720f4b419b07c77141bd1cbe7
Replit user <>
2023-09-29 13:03:45 +0000

    replit hosting

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const express = require('./config/express');
 const app = express();
+
+
 app.listen(3000);
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
+
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+
+keepAlive();
+const monitor = new Monitor({
+	website: '',
+	title: 'NAME',
+	interval: 2
+});
+
+monitor.on('up', (res) => console.log(`${res.website} its on.`));
+monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} has stopped.`));
+monitor.on('error', (error) => console.log(error));
